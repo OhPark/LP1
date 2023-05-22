56fc858ab87e86319429820d173153cc9256c3b4
ping9oo <fpdl96@gmail.com>
2023-05-22 18:02:32 +0900

    add article update function incomplete comments create and get comment list

--- OhPark-pjt/final-pjt-back/communities/serializers.py
+++ OhPark-pjt/final-pjt-back/communities/serializers.py
@@ -3,16 +3,18 @@ from .models import Article, Comment
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-
+    user_name = serializers.CharField(source='user.username', read_only=True)
     # image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Article
         # fields = ('id', 'title', 'content', 'image',)
-        fields = ('id', 'title', 'content',)
+        fields = ('id', 'title', 'content', 'user_name', 'created_at', 'updated_at',)
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    comments = ArticleSerializer(many=True, read_only=True)
+
     class Meta:
 
         model = Comment
